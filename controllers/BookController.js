61046f7b2023e4007cbfc66c010b14ba5b888896
esadeli <efrat.sadeli@gmail.com>
2018-10-02 16:24:45 +0700

    Book CRUD OK

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -10,7 +10,7 @@ class BookController{
             isbn: req.body.isbn,
             title: req.body.title,
             author: req.body.author,
-            category: req.body.author,
+            category: req.body.category,
             stock: Number(req.body.stock)
         })
         .then(book=>{
@@ -20,6 +20,64 @@ class BookController{
             })
         })
         .catch(error=>{
+            res.status(500).json({
+                msg: 'ERROR: ',error
+            })
+        })
+    }
+
+    // get list of books
+    static getListOfBooks(req,res){
+        Book.find({})
+            .then(books=>{
+                res.status(200).json({
+                    msg: 'List of Books',
+                    data: books
+                })
+            })
+            .catch(error=>{
+                res.status(500).json({
+                    msg: 'ERROR: ',error
+                })
+            })
+    }
+
+    //edit book information
+    static updateBookById(req,res){
+        Book.findOneAndUpdate({
+            _id: req.params.id
+        },{
+            isbn: req.body.isbn,
+            title: req.body.title,
+            author: req.body.author,
+            category: req.body.category,
+            stock: Number(req.body.stock)
+        })
+        .then(book=>{
+            res.status(200).json({
+                msg: 'Book has been updated',
+                data: book
+            })
+        })
+        .catch(error =>{
+            res.status(500).json({
+                msg: 'ERROR: ',error
+            })
+        })
+    }
+
+    // delete book information
+    static deleteBookById(req,res){
+        Book.findOneAndRemove({
+            _id: req.params.id
+        })
+        .then(book =>{
+            res.status(200).json({
+                msg: 'Book has been deleted',
+                data: book
+            })
+        })
+        .catch(error =>{
             res.status(500).json({
                 msg: 'ERROR: ',error
             })
--- routes/BookRouter.js
+++ routes/BookRouter.js
@@ -9,5 +9,19 @@ router.post('/', (req,res)=>{
     BookController.createBook(req,res)
 })
 
+// get list of books
+router.get('/lists', (req,res)=>{
+    BookController.getListOfBooks(req,res)
+})
+
+// edit Book information
+router.put('/:id', (req,res) =>{
+    BookController.updateBookById(req,res)
+})
+
+// delete Book information
+router.delete('/:id', (req,res) =>{
+    BookController.deleteBookById(req,res)
+})
 
 module.exports = router
