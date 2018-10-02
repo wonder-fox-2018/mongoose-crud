be4ae468882f3fd1e5f54a2b94666acfcb47f23d
fmurtadho <fmurtadho@gmail.com>
2018-10-02 16:55:03 +0700

    release 1 done

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,7 +2,7 @@ const Book = require('../models/book');
 
 class Controller {
 
-    static add(req,res){
+    static create(req,res){
         Book.create({ 
             isbn:  req.body.isbn,
             title:  req.body.title,
@@ -19,6 +19,51 @@ class Controller {
             }
           });
     }
+
+    static read(req,res){
+        Book.find({}, function (err, docs) {
+            if(err){
+                res.status(400).json({
+                    message : 'error read all book data'
+                })
+            }else{
+                res.json(docs)
+            }
+        });
+    }
+
+    static update(req,res){
+        Book.updateOne({_id : req.params.id},{$set:{
+            isbn:  req.body.isbn,
+            title:  req.body.title,
+            author: req.body.author,
+            category: req.body.category,
+            stock: req.body.stock
+        }}, function(err, update) {
+            if(err){
+                res.status(400).json({
+                    message : 'update error'
+                })
+            }else{
+                res.status(200).json(req.body)
+            }
+        });
+    }
+    
+    static delete(req,res){
+        Book.deleteOne({_id : req.params.id},function(err,r){
+            if(err){
+                res.status(400).json({
+                    message : `failed to delete ${req.params.id}`
+                })
+            }else{
+                res.status(200).json({
+                    message : `delete ${req.params.id} success`
+                })
+            }
+        })
+    }
+
 }
 
 module.exports = Controller;
--- routes/books.js
+++ routes/books.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 const bookController = require('../controllers/bookController')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('HELO FROM BOOKS')
-});
+router.post('/add',bookController.create)
 
-router.post('/add',bookController.add)
+router.get('/', bookController.read);
+
+router.put('/:id',bookController.update)
+
+router.delete('/:id',bookController.delete)
 
 module.exports = router;
