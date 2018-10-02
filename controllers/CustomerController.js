8c3a29636a049e776871655924e31c93d267b7f4
esadeli <efrat.sadeli@gmail.com>
2018-10-02 16:46:30 +0700

    Customer CRUD OK

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -25,6 +25,64 @@ class CustomerController{
             })
         })
     }
+
+    // get list of customers
+    static getListOfCustomers(req,res){
+        Customer.find({})
+         .then(customers =>{
+            res.status(200).json({
+                msg: 'List of customers',
+                data: customers
+            })
+         })
+         .catch(error =>{
+            res.status(500).json({
+                msg: 'ERROR: ',error
+            })
+         })
+    }
+
+    // update customer data
+    static updateCustomerById(req,res){
+        Customer.findOneAndUpdate({
+            _id: req.params.id
+        },{
+            name: req.body.name,
+            memberid: req.body.memberid,
+            address: req.body.address,
+            zipcode: req.body.zipcode,
+            phone: req.body.phone
+        })
+        .then(customer =>{
+            res.status(200).json({
+                msg: 'Customer data has been updated',
+                data: customer
+            })
+        })
+        .catch(error =>{
+            res.status(500).json({
+                msg: 'ERROR: ',error
+            })
+        })
+    }
+
+    // delete customer data
+    static deleteCustomerById(req,res){
+        Customer.findOneAndRemove({
+            _id: req.params.id
+        })
+        .then(customer =>{
+            res.status(200).json({
+                msg: 'Customer data has been deleted',
+                data: customer
+            })
+        })
+        .catch(error =>{
+            res.status(500).json({
+                msg: 'ERROR: ',error
+            })
+        })
+    }
 }
 
 module.exports = CustomerController
--- routes/CustomerRouter.js
+++ routes/CustomerRouter.js
@@ -9,5 +9,19 @@ router.post('/', (req,res) =>{
     CustomerController.createCustomer(req,res)
 })
 
+// get lists of customer
+router.get('/lists', (req,res) =>{
+    CustomerController.getListOfCustomers(req,res)
+})
+
+// update customer data
+router.put('/:id', (req,res) =>{
+    CustomerController.updateCustomerById(req,res)
+})
+
+// delete customer data
+router.delete('/:id', (req,res)=>{
+    CustomerController.deleteCustomerById(req,res)
+})
 
 module.exports = router
